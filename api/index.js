8774ff1edf5a5b908aa7227438de495d34fea1d4
Minibumphead <minibumphead281@gmail.com>
2021-01-31 14:04:51 +0100

    test

--- api/index.js
+++ api/index.js
@@ -5,17 +5,19 @@ import cors from 'cors'
 
 import todosRouter from './routes/todos.js'
 import usersRouter from './routes/users.js'
+import dotenv from 'dotenv'
+
 
 // setting up express 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
+dotenv.config()
 
 // setting port and db connection
 const PORT = 5000
-const DB_CONNECTION = "mongodb+srv://Minibumphead:catdogmouse!!!999@cluster0.5gnyy.mongodb.net/wg_db?retryWrites=true&w=majority"
 
-mongoose.connect(DB_CONNECTION,  { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false})
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5gnyy.mongodb.net/wg_db?retryWrites=true&w=majority`,  { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false})
 .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
 .catch((err)=>console.log(err))
 
